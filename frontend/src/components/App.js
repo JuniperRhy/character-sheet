7681c02ca386679b8fd2d895a6feef8182a26091
JuniperRhy <83832965+JuniperRhy@users.noreply.github.com>
2021-08-03 18:34:44 -0500

    Update App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,16 +2,15 @@ import Header from "./Header";
 import MainPage from "./MainPage";
 import { useEffect } from "react";
 
-
 function App() {
   useEffect(() => {
     fetch("http://localhost:9292").then((response) => console.log("hello!"));
   });
 
   return (
-    <div className="App">
-        <Header />
-        <MainPage />
+    <div className="Application">
+      <Header />
+      <MainPage />
     </div>
   );
 }
