cb48e31b79d219316e220c813865d8ea965fd932
JuniperRhy <83832965+JuniperRhy@users.noreply.github.com>
2021-08-03 18:36:58 -0500

    Update Character.js

--- frontend/src/components/Character.js
+++ frontend/src/components/Character.js
@@ -1,21 +1,21 @@
-import { useParams } from "react-router-dom"
-
+import { useParams } from "react-router-dom";
+import CharacterCreator from "./CharacterCreator";
+import { NavLink } from "react-router-dom";
 
 function Character() {
+  const paramsPort = useParams();
 
-    const paramsPort = useParams()
+  console.log(paramsPort);
 
-    console.log(paramsPort)
-
-    return (
+  return (
     <div>
-        <h1>My Character</h1>
-       <h2>Test characters</h2>
-           
+      <NavLink to="/charactercreator">
+        <h2>✦Create New Character✦ </h2>
+      </NavLink>
+      <h1>My Characters</h1>
+      <h2>Test characters</h2>
     </div>
-    )
-
+  );
 }
 
-
-export default Character
+export default Character;
