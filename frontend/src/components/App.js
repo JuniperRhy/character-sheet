cf0270c3d7a38ab89ee43219b0d1ac76fbd55960
cocktailcoder <iokreddit@protonmail.com>
2021-08-03 23:17:00 -0400

    server side request successful

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,9 +3,9 @@ import MainPage from "./MainPage";
 import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    fetch("http://localhost:9292").then((response) => console.log("hello!"));
-  });
+  // useEffect(() => {
+  //   fetch("http://localhost:9292").then((response) => console.log("hello!"));
+  // });
 
   return (
     <div className="Application">
--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -1,10 +1,22 @@
 import { Switch, Route } from "react-router-dom"
 import Character from "./Character"
 import Campaign from "./Campaign"
-
+import {useState, useEffect} from 'react';
 
 function MainPage () {
-    return (
+  const [playersData, playersDataSetter] = useState([])
+  const [players, playersSetter] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:9393/players")
+      .then((response) => response.json())
+      .then(players => {
+        console.log(players);
+        playersDataSetter(players);
+        playersSetter(players)
+      })
+  }, [])
+
+return (
 <div className="App">
 <h1>Login</h1>
 <Switch>
