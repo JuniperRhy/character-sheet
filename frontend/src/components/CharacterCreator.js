764d523add78037e3a1a1770b70cf9480865ef0d
JuniperRhy <gwyniver.rhy@gmail.com>
2021-08-04 11:43:56 -0500

    added creators

--- frontend/src/components/CharacterCreator.js
+++ frontend/src/components/CharacterCreator.js
@@ -3,11 +3,84 @@ import { useParams } from "react-router-dom";
 function CharacterCreator() {
   const paramsPort = useParams();
 
-  console.log(paramsPort, "Hello");
+  console.log(paramsPort, "Test");
 
   return (
-    <div>
-      <h1>Character creator</h1>
+    <div className="charactercreator">
+      <h2>Character Creator</h2>
+      <form>
+        <label>
+          ✦ Character name:
+          <input className="inputfield" type="text" />
+        </label>
+        <br></br>
+        <label>
+          ✦ Race:
+          <input className="inputfield" type="text" />
+        </label>
+        <br></br>
+        <label>
+          ✦ Class:
+          <input className="inputfield" type="text" />
+        </label>
+        <br></br>
+        <label>
+          ✦ Alignment:
+          <input className="inputfield" type="text" />
+        </label>
+        <br></br>
+        <label>
+          ✦ Strength:
+          <input className="inputfield" type="text" />
+        </label>
+        <br></br>
+        <label>
+          ✦ Dexterity:
+          <input className="inputfield" type="text" />
+        </label>
+        <br></br>
+        <label>
+          ✦ Constitution:
+          <input className="inputfield" type="text" />
+        </label>
+        <br></br>
+        <label>
+          ✦ Intelligence:
+          <input className="inputfield" type="text" />
+        </label>
+        <br></br>
+        <label>
+          ✦ Wisdom:
+          <input className="inputfield" type="text" />
+        </label>
+        <br></br>
+        <label>
+          ✦ Charisma:
+          <input className="inputfield" type="text" />
+        </label>
+        <br></br>
+        <label>
+          ✦ Personality:
+          <input className="inputfield" type="text" />
+        </label>
+        <br></br>
+        <label>
+          ✦ Traits:
+          <input className="inputfield" type="text" />
+        </label>
+        <br></br>
+        <label>
+          ✦ Flaws:
+          <input className="inputfield" type="text" />
+        </label>
+        <br></br>
+        <label>
+          ✦ Equipment:
+          <input className="inputfield" type="text" />
+        </label>
+        <br></br>
+        <input className="submitbtn" type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -1,34 +1,42 @@
-import { Switch, Route } from "react-router-dom"
-import Character from "./Character"
-import Campaign from "./Campaign"
-import {useState, useEffect} from 'react';
+import { Switch, Route } from "react-router-dom";
+import Character from "./Character";
+import Campaign from "./Campaign";
+import CharacterCreator from "./CharacterCreator";
+import CampaignCreator from "./CampaignCreator";
+import { useState, useEffect } from "react";
 
-function MainPage () {
-  const [playersData, playersDataSetter] = useState([])
+function MainPage() {
+  const [playersData, playersDataSetter] = useState([]);
   const [players, playersSetter] = useState([]);
   useEffect(() => {
     fetch("http://localhost:9393/players")
       .then((response) => response.json())
-      .then(players => {
+      .then((players) => {
         console.log(players);
         playersDataSetter(players);
-        playersSetter(players)
-      })
-  }, [])
+        playersSetter(players);
+      });
+  }, []);
 
-return (
-<div className="App">
-<h1>Login</h1>
-<Switch>
-<Route path="/campaign">
-  <Campaign />
-</Route>
-<Route exact path="/character" >
-  <Character />
-</Route>
-</Switch>
-</div>
-)
-    }
+  return (
+    <div className="App">
+      {/* <h1>Login</h1> */}
+      <Switch>
+        <Route path="/campaign">
+          <Campaign />
+        </Route>
+        <Route exact path="/character">
+          <Character />
+        </Route>
+        <Route exact path="/charactercreator">
+          <CharacterCreator />
+        </Route>
+        <Route exact path="/campaigncreator">
+          <CampaignCreator />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
 
-export default MainPage
+export default MainPage;
