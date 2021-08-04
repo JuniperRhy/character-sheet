81301b77fc9de0ff24af3d4ff5f37e49d99d5ef5
JuniperRhy <gwyniver.rhy@gmail.com>
2021-08-04 16:02:36 -0500

    add frontend login

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,20 @@
 import Header from "./Header";
 import MainPage from "./MainPage";
 import { useEffect } from "react";
+import React from "react";
+import "./App.css";
+import Login from "./Login/Login";
+import useToken from "./useToken";
 
 function App() {
-  // useEffect(() => {
-  //   fetch("http://localhost:9292").then((response) => console.log("hello!"));
-  // });
+  useEffect(() => {
+    fetch("http://localhost:9292").then((response) => console.log("hello!"));
+  });
+  const { token, setToken } = useToken();
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
 
   return (
     <div className="Application">
