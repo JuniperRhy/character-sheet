6100594a4713555d2882f3b83415469d9e207dc2
cocktailcoder <iokreddit@protonmail.com>
2021-08-05 10:38:22 -0400

    login attempt

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -5,10 +5,7 @@ import CharacterCreator from "./CharacterCreator";
 import CampaignCreator from "./CampaignCreator";
 import { useState, useEffect } from "react";
 
-<<<<<<< HEAD
 function MainPage () {
-  // const [playersData, playersDataSetter] = useState([])
-  // const [players, playersSetter] = useState([]);
 
   const [chars, setChars] = useState([])
   const [myCharacters, setMyCharacters] = useState([])
@@ -45,48 +42,22 @@ return (
       <Route path="/campaign">
         <Campaign />
       </Route>
+
       <Route path="/character" component= {
             () => <Character  chars = {chars} myCharacters={myCharacters}/>
           }>
       </Route>
-    </Switch>
+
+      <Route exact path="/charactercreator">
+            <CharacterCreator />
+      </Route>
+
+      <Route exact path="/campaigncreator">
+            <CampaignCreator />
+      </Route>
+        </Switch>
 </div>
 )
     }
-=======
-function MainPage() {
-  const [playersData, playersDataSetter] = useState([]);
-  const [players, playersSetter] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:9393/players")
-      .then((response) => response.json())
-      .then((players) => {
-        console.log(players);
-        playersDataSetter(players);
-        playersSetter(players);
-      });
-  }, []);
-
-  return (
-    <div className="App">
-      {/* <h1>Login</h1> */}
-      <Switch>
-        <Route path="/campaign">
-          <Campaign />
-        </Route>
-        <Route exact path="/character">
-          <Character />
-        </Route>
-        <Route exact path="/charactercreator">
-          <CharacterCreator />
-        </Route>
-        <Route exact path="/campaigncreator">
-          <CampaignCreator />
-        </Route>
-      </Switch>
-    </div>
-  );
-}
->>>>>>> a0744b2de3288e0bf23e1a9b17cdbfaa6f40555b
 
 export default MainPage;
