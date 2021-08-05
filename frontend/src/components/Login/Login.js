a0744b2de3288e0bf23e1a9b17cdbfaa6f40555b
JuniperRhy <gwyniver.rhy@gmail.com>
2021-08-05 09:17:29 -0500

    delete DO NOT USE/BACKEND

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -31,17 +31,24 @@ export default function Login({ setToken }) {
       <form onSubmit={handleSubmit}>
         <label>
           <p>Username</p>
-          <input type="text" onChange={(e) => setUserName(e.target.value)} />
+          <input
+            className="input-field"
+            type="text"
+            onChange={(e) => setUserName(e.target.value)}
+          />
         </label>
         <label>
           <p>Password</p>
           <input
+            className="input-field"
             type="password"
             onChange={(e) => setPassword(e.target.value)}
           />
         </label>
         <div>
-          <button type="submit">Submit</button>
+          <button className="submitbtn" type="submit">
+            Submit
+          </button>
         </div>
       </form>
     </div>
